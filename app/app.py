bd99f87183be49c21143109077e983b7fa505f54
Sam Hoile <samuel.hoile@gmail.com>
2026-03-15 10:14:19 +0000

    added documentation

--- app/app.py
+++ app/app.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
@@ -11,7 +13,14 @@ from app.webapp_routes.monitor_button_routes import monitor_buttons_router
 from app.webapp_routes.monitor_ws_routes import monitor_ws_router
 from app.webapp_routes.player_button_routes import player_buttons_router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 app.include_router(receivers_router)
 app.include_router(receiver_buttons_router)
@@ -22,9 +31,7 @@ app.include_router(monitor_router)
 app.include_router(monitor_buttons_router)
 app.include_router(monitor_ws_router)
 
+
 @app.get("/")
 def read_root(request: Request):
     return RedirectResponse(url=request.url_for("read_monitor"))
-
-
-create_db_and_tables()
